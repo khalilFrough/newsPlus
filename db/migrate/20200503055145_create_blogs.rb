3790a4350ade69ad00c010ec0c357302ec941380
khalil Frough <khalil.froughkmu@gmail.com>
2020-05-03 15:55:12 +1000

    created the blog model

---
+++ db/migrate/20200503055145_create_blogs.rb
@@ -0,0 +1,13 @@
+class CreateBlogs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.text :body
+      t.integer :user_id
+      t.string :view_count
+      t.string :topic
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_051646) do
+ActiveRecord::Schema.define(version: 2020_05_03_055145) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.string "view_count"
+    t.string "topic"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
